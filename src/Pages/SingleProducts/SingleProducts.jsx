a3580a31da9e1b6516911b07f1fc95da7056dac2
alimardon28 <doniyorovalimardon369@gmail.com>
2022-04-21 05:40:14 +0500

    added singleProducts pages products

--- src/Pages/SingleProducts/SingleProducts.jsx
+++ src/Pages/SingleProducts/SingleProducts.jsx
@@ -3,8 +3,9 @@ import { useContext , useEffect , useState } from 'react';
 import { Link , useParams } from 'react-router-dom';
 import heart from "../../Components/assets/images/heart.svg";
 import heartFill from "../../Components/assets/images/heart-fill.svg";
-import cart from "../../Components/assets/images/cart3.svg";
 import { Context } from '../../Context/Context';
+import Button from '@mui/material/Button';
+import '../SingleProducts/SingleProducts.css';
 
 
 
@@ -25,10 +26,11 @@ const Singleproducts = () => {
 
 
    useEffect(() => {
-    fetch(`https://authentiication.herokuapp.com/posts/:${date.productId}`)
+    fetch(`https://authentiication.herokuapp.com/posts/${date.productId}`)
     .then(response => response.json())
     .then(data => setSingleProducts(data))
-     } , [ setSingleProducts ]);
+
+     } , [])
 
 
 
@@ -36,7 +38,37 @@ const Singleproducts = () => {
         <>
             <section className="sectionProducts">
                 <div className="container">
-                    
+                     <div className="single-products">
+                         <div className="singleHeartBox">
+                             <button className='heartBtn'>
+                                 <img src={heart} alt="images heart" />
+                             </button>
+                         </div>
+                         <div className="imagesBox">
+                             <img src={singleProducts.thumbnailUrl} alt={`${singleProducts.title} images`} />
+                             <img src={singleProducts.thumbnail} alt={`${singleProducts.title} images`} />
+                         </div>
+                         <div className="sumBox">
+                             <h2 className='sumBox-title'>{singleProducts.title}</h2>
+                             <p className='sumBox-sum'>$ {singleProducts.sum}</p>
+                         </div>
+                     </div>
+                    <div className="sinleProductsBox">
+                        <div className="sinleProductsBox-left">
+                            <div className="opitsaneBox">
+                                <h3 className='opisaneBox-heading'>Описание и характеристики</h3>
+                            </div>
+                            <div className="opitsane-description">
+                                <p className='opitsane-desc'>{singleProducts.description}</p>
+                            </div>
+                        </div>
+                        <div className="buttonBox">
+                            <Button  className='button' variant="contained">купить !</Button>
+                            <Button  className='button' variant='contained'>
+                            <i className="bi bi-cart3"></i>
+                                добавить в корзину</Button>
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
