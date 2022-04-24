524500e14668199220fbfbb6c9a34bf3377f03aa
alimardon28 <doniyorovalimardon369@gmail.com>
2022-04-24 12:57:54 +0500

    finished ordered pages responsive

--- src/Pages/Ordered/Ordered.jsx
+++ src/Pages/Ordered/Ordered.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import '../Ordered/Ordered.css';
-import gif from '../../Components/assets/images/happy.gif';
-
+import React from "react";
+import "../Ordered/Ordered.css";
+import gif from "../../Components/assets/images/happy.gif";
 
 const Ordered = () => {
-
-
-
-
-    return (
-        <div className='container'>
-            <div className="orderidHero">
-
-            <img className='orderidHero-gif' src={gif} alt="happy gif" />
-                 <div className="orderedBox">
-                     <p className='ordered-desc'>Номер вашего заказа  <span className='ordered-desc-span'>№ {Math.floor(Math.random() * 1000000)}</span> , с Вами свяжется наш менеджер.</p>
-                 </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="orderidHero">
+        <img className="orderidHero-gif" src={gif} alt="happy gif" />
+        <div className="orderedBox">
+          <p className="ordered-desc">
+            Номер вашего заказа{" "}
+            <span className="ordered-desc-span">
+              № {Math.floor(Math.random() * 1000000)}
+            </span>{" "}
+            , с Вами свяжется наш менеджер.
+          </p>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Ordered;
